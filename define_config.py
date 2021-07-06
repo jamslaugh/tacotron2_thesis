90b05c1f2c2328f767c4df396bdf90d0c26d0c5e
Giacomo Matrone <gia.matrone@gmail.com>
2021-07-06 12:55:41 +0200

    created define_config.py

---
+++ define_config.py
@@ -0,0 +1,8 @@
+import os
+
+if __name__ == '__main__':
+    files_list = [el.split('.')[0] for el in os.listdir(os.path.join('Data EAF','materials')) if el.endswith('.eaf')]
+    print(os.getcwd(),files_list)
+    with open('config_file.txt','w+') as file:
+        file.write('\n'.join(files_list))
+        file.close()
