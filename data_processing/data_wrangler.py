746b81d1fc06f8ba3048d43d29a85a861270847c
Giacomo Matrone <gia.matrone@gmail.com>
2021-08-16 13:27:48 +0200

    added optionable conciliation

--- data_processing/data_wrangler.py
+++ data_processing/data_wrangler.py
@@ -44,23 +44,32 @@ def DataLoad(filename,path,keys_to_search=['dstr','ORT'],text_file = False):
 
     return OUTPUT_DICT
 
-def data_wrangle(output_dict,token=True):
+def data_wrangle(output_dict,token=True,conciliation = False):
     """
     This function is used to wrangle data and prepare the output for the Tacotron 2 algorythm
     :param output_dict: a dictionary containing dstr and ort keys
     :param token: bool type, if the <PRL> token has to be inserted or not. If False inserts the prolongation per se.
+    :param conciliation: bool type, to be used only if you want annotation levels ORT and dstr to match. Note that if token = True, then conciliation = True
     :return: ort_data_new a Data Frame with processed data
     """
-    conc_data = conciliator(output_dict['dstr'],output_dict['ort'])
-    ort_data_new = inserted_df(output_dict['ort'])
-    ort_data_new = time_conciliation(ort_data_new,conc_data)
+    if token:
+        conciliation = True
+
+    ort_data_new = output_dict['ort']
+    if conciliation:
+        ort_data_new = inserted_df(ort_data_new)
+        conc_data = conciliator(output_dict['dstr'], output_dict['ort'])
+        ort_data_new = time_conciliation(ort_data_new,conc_data)
     ort_data_new['Duration'] = ort_data_new['End Time'] - ort_data_new['Begin Time']
     if token:
         ort_data_new.ORT = ort_data_new.ORT.fillna('<PRL>')
     else:
         ort_data_new.ORT = ort_data_new.ORT.fillna(ort_data_new.iloc[:,0].dropna())
-    ort_data_new.ORT = ort_data_new.ORT.str.replace(r'(\w+)(\s)(\<.*\>)', r'\1')
-    ort_data_new.ORT = ort_data_new.ORT.str.replace(r'(\w+)(\<.*\>)', r'\1')
+    if conciliation:
+        ort_data_new.ORT = ort_data_new.ORT.str.replace(r'(\w+)(\s)(\<.*\>)', r'\1')
+        ort_data_new.ORT = ort_data_new.ORT.str.replace(r'(\w+)(\<.*\>)', r'\1')
+    else:
+        ort_data_new.ORT = ort_data_new.ORT.str.replace(r'(\w+)(\s)(\<)(.*)(\>)', r'\1\4').str.replace(r'(\w+)(\<)(.*)(\>)',r'\1\3')
     return ort_data_new
     #ort_data_new.to_csv(os.path.join('../preprocessing_results', 'trial.csv'))
     # filename G01V01P01, ..\..\Data EAF\Training Data EAF
--- main.py
+++ main.py
@@ -17,8 +17,10 @@ args.add_argument('--cfg_path',dest='cfg_file_path',default='config_file.txt',
                   help='The cfg file contains the filenames to be processed')
 args.add_argument('--debug',dest='debug',default=False,type=bool,
                   help='Debug mode')
+args.add_argument("--conciliation",dest='conc', type=bool,
+                  help='Conciliation if annotation needs to be matched from one annotation level to another')
 
-def folder_reader(file_path,config_file_path='config_file.txt',text_file=True,token=True,research_keys=['dstr', 'ORT'],debug=False):
+def folder_reader(file_path,config_file_path='config_file.txt',text_file=True,token=True,research_keys=['dstr', 'ORT'],debug=False,conc = False):
 
     with open(config_file_path,'r+') as file:
         files_list = file.read().split('\n')
@@ -42,24 +44,26 @@ def folder_reader(file_path,config_file_path='config_file.txt',text_file=True,to
             out_filename = out_filename + ".eaf"
             out_dict = DataLoad(out_filename, file_path, keys_to_search=research_keys, text_file=False)
 
+
         if token:
 
-            ort_data_new = data_wrangle(out_dict, token=True)
+            ort_data_new = data_wrangle(out_dict, token=True,conciliation = conc)
 
         else:
 
-            ort_data_new = data_wrangle(out_dict, token=False)
+            ort_data_new = data_wrangle(out_dict, token=False,conciliation = conc)
 
         ort_data_new['wav_reference'] = wav_reference[_] + '.wav'
-        ort_data_new = ort_data_new.drop(0,axis=1)
+        if conc:
+            ort_data_new = ort_data_new.drop(0,axis=1)
 
         try:
             ort_data_new['Begin Time'] = ort_data_new['Begin Time'].astype('int64')
             ort_data_new['End Time'] = ort_data_new['End Time'].astype('int64')
         except:
-            raise Exception('Error in data, couldn\'t convert "Begin Time" and "End Time" format to int 64.\n\n There are %d missing values in "Begon Time" and %d in "End Time"\n This is the traceback:\n\n%s'%(ort_data_new['Begin Time'].isna().sum(),ort_data_new['End Time'].isna().sum(),sys.exc_info()[1]))
+            raise Exception('Error in data, couldn\'t convert "Begin Time" and "End Time" format to int 64.\n\n There are %d missing values in "Begin Time" and %d in "End Time"\n This is the traceback:\n\n%s'%(ort_data_new['Begin Time'].isna().sum(),ort_data_new['End Time'].isna().sum(),sys.exc_info()[1]))
         if 'phr' in research_keys:
-            ort_data_new = data_all = pd.merge_asof(ort_data_new,out_dict['phr'],on='Begin Time',suffixes = ['','_phr'])
+            ort_data_new = pd.merge_asof(ort_data_new,out_dict['phr'],on='Begin Time',suffixes = ['','_phr'])
         df.append(ort_data_new)
 
     df = pd.concat(df)
@@ -76,7 +80,8 @@ if __name__ == '__main__':
     research_keys = arguments.research_keys
     cfg_path = arguments.cfg_file_path
     debug = arguments.debug
-    ort_data_new=folder_reader(out_path,cfg_path,text_file,token,research_keys,debug)
+    conciliation = arguments.conc
+    ort_data_new=folder_reader(out_path,cfg_path,text_file,token,research_keys,debug,conciliation)
     ort_data_new.to_csv(os.path.join('', 'preprocessing_results/trial.csv'),index=False)
 
 # if __name__ == '__main__':
